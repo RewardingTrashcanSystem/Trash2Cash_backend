13d9843e4ecc348f88308faef59cdb18bfa4af3c
Mekdi-kassa <mekdelawitkassa6@gmail.com>
2025-12-24 11:22:24 +0300

    all done

--- user/serializers.py
+++ user/serializers.py
@@ -54,3 +54,19 @@ class LoginSerializer(serializers.Serializer):
 
         data['user'] = authenticated_user
         return data
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id', 'email', 'first_name', 'last_name',
+            'phone_number', 'image', 'total_points', 'eco_level', 'user_type'
+        ]
+        read_only_fields = ['id', 'email', 'total_points', 'eco_level', 'user_type']
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+        instance.image = validated_data.get('image', instance.image)
+        instance.save()
+        return instance
--- user/views.py
+++ user/views.py
@@ -2,9 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import RegisterSerializer, LoginSerializer
+from .serializers import RegisterSerializer, LoginSerializer,ProfileSerializer
 from rest_framework.parsers import MultiPartParser, FormParser
-from .serializers import ProfileSerializer
+
 
 class RegisterAPIView(APIView):
     permission_classes = [permissions.AllowAny]
